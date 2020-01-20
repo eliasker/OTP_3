73de807045b9513822742f11cfc6d7e9ecb6145c
jarr <jesse@arola.fi>
2020-01-20 11:52:45 +0200

    Added backend skeleton

---
+++ kaiku_back/src/main/java/Ryhma_3/Kaiku_BE/App.java
@@ -0,0 +1,13 @@
+package Ryhma_3.Kaiku_BE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ){
+
+        System.out.println( "Hello World!" );
+
+    }
+}
