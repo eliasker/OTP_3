bea4778c1e11f49a2c93c008c46d49576989f3bf
Kaikuh <leevi.niemi@metropolia.fi>
2020-01-31 15:34:25 +0200

    added socket connection to the chat client

--- kaiku_front/src/App.js
+++ kaiku_front/src/App.js
@@ -2,14 +2,35 @@ import React, { useState } from 'react'
 import './App.css'
 import Login from './Login'
 import Chat from './Chat'
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:6969");
 
 const App = () => {
+  
   const [currentPage, setCurrentPage] = useState('login')
+  const [msg, setMsg] = useState('')
 
+  const handleMsgChange = e => {
+    setMsg(e.target.value)
+  }
+  
+  const onSubmit = () => {
+    socket.emit("message", msg)
+    setMsg('')
+  }
 
   return (
-    <div className="App">
-      {currentPage === 'a' ? <Login/>: <Chat />}
+
+    //   <div className="App">
+    //       {currentPage === 'a' ? <Login/>: <Chat />}
+    //   </div>
+
+
+    //test input for messages
+    <div>
+      <input onChange={e => handleMsgChange(e)} value={msg} />
+      <button onClick={onSubmit}>Send</button>
     </div>
   )
 }
