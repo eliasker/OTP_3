5ea786f3d2657ca9751c4aeed25d6a21cf943c7e
Kaikuh <leevi.niemi@metropolia.fi>
2020-02-02 17:17:43 +0200

    socket hook

--- kaiku_front/src/Chat.js
+++ kaiku_front/src/Chat.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import UsersColumn from './components/UsersColumn'
 import ChatColumn from './components/ChatColumn'
+import useChat from "./hooks/useChat"
 
 const Chat = ({ users, messages, setMessages, loggedUser }) => {
-	return (
-		<div id="chat" className="container">
-			<div className="chat-container container row">
-				<UsersColumn users={users} />
-				<ChatColumn messages={messages} setMessages={setMessages} loggedUser={loggedUser} users={users} />
-			</div>
-		</div>
-	)
+
+  const { sendMessage } = useChat()
+  
+  return (
+    <div id="chat" className="container">
+      <div className="chat-container container row">
+	<UsersColumn users={users} />
+	<ChatColumn messages={messages} setMessages={setMessages} loggedUser={loggedUser} users={users} sendMessage={message => {
+          sendMessage({ message });
+        }}/>
+      </div>
+    </div>
+  )
 }
 
 export default Chat
