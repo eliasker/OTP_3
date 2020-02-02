12abe9fd656417b9e852859f6593bc312a86cc2c
jarr <jesse@arola.fi>
2020-02-02 13:37:00 +0200

    Redid the base project with reference Jersey project

---
+++ kaiku_back/src/main/java/ryhma_3/Account.java
@@ -0,0 +1,16 @@
+package ryhma_3;
+
+/**
+ * Account
+ */
+public class Account {
+    private String username;
+    private String fullName;
+    private String password;
+
+    public Account(String username, String fullName, String password) {
+        this.username = username;
+        this.fullName = fullName;
+        this.password = password;
+    }
+}
