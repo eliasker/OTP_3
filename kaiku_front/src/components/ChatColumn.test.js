e5190ebf001ce488c7d99f10e661aa09cf05fd98
eliasker <eliasker@gmail.com>
2020-02-11 18:34:03 +0200

    testing libraries added

---
+++ kaiku_front/src/components/ChatColumn.test.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, cleanup } from '@testing-library/react'
+import InMessage from './InMessage'
+import OutMessage from './OutMessage'
+import messageValidation from '../util/inputValidation'
+import jsonService from '../services/jsonService'
+
+afterEach(cleanup)
+
+test('renders inMessage from other users and outMessage from loggedUser', () => {
+  const testUser = {
+    name: 'TestUser',
+    id: 1,
+    color: 'red'
+  }
+  const loggedUser = {
+    name: 'LoggedUser',
+    id: 2,
+    color: 'green'
+  }
+
+  const messages = [{
+    content: 'Message from TestUser',
+    id: 1,
+    user_id: testUser.id
+  },
+  {
+    content: 'Message from LoggedUser',
+    id: 2,
+    user_id: loggedUser.id
+  }]
+
+  const component = render(
+    <InMessage user={testUser} content={messages[0].content} />
+  )
+  component.debug()
+
+  expect(component.container).toHaveTextContent(
+    'Message from TestUser'
+  )
+})
