f06f10dbb7a531530359723714e3bf3ec9dc746a
wesenbergg <b.b.j.1998@gmail.com>
2020-02-22 15:43:38 +0200

    added e2e tests

--- kaiku_front/cypress/integration/app.spec.js
+++ kaiku_front/cypress/integration/app.spec.js
@@ -1,7 +1,7 @@
-describe('Browser', function() {
+describe('Basic functionality', function() {
 
   beforeEach(function() {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
   })
 
   it('front page can be opened', function() {
@@ -23,10 +23,7 @@ describe('Browser', function() {
       cy.get('#inputUsername')
         .type('123')
       cy.get('#inputPassword')
-        .type('123')
-      cy.get('.btn')
-        .click()
-      cy.contains('Hissichat')
+        .type('123{enter}')
     })
 
     it('display own profile works', function() {
@@ -36,10 +33,38 @@ describe('Browser', function() {
       cy.contains('Tallenna')
     })
 
-    it('display own profile works', function() {
-      cy.get('.fa-times').click()
+    it('check profile then type message', function() {
+      cy.get('.fa-ellipsis-v').click()
+      cy.get('.dropdown-item').first().click()
       cy.contains('Poistu')
-      cy.contains('Tallenna')
+      cy.get('.fa-times').click()
+      cy.get('.message-from-group').type('123 check check')
+      cy.get('.send-btn').click()
+      cy.get('.out-message').last().contains('123 check check')
+    })
+
+    it('check display private chat list', function() {
+      cy.get('.fa-user-friends').click()
+      cy.get('.profile').last().contains('It is a long established fact that')
+    })
+
+    it('check search messages', function() {
+      cy.get('#search-message').click()
+      cy.focused().type('vastaus')
+      cy.get('.in-container').should('have.length', 4)
+    })
+
+    it('check search users', function() {
+      cy.get('.fa-user-friends').click()
+      cy.get(".profile").should('have.length', 6)
+      cy.get('.find-user-input').click().type('Mirka')
+      cy.get(".profile").should('have.length', 1)
+    })
+
+    it('check log out', function() {
+      cy.get('.fa-ellipsis-v').click()
+      cy.get('.fa-door-open').click()
+      cy.contains('Kirjaudu')
     })
   })
 })
