1208dbc1f8b057c54f60ecdeebe43ce71c3e0ea7
Panu Lidnqvist <panu.lindqvist@gmail.com>
2020-02-25 13:16:59 +0200

    Implemented groupchatting functionality with socket.io's multiplexing feature.

--- kaiku_back/src/main/java/com/ryhma_3/kaiku/socket/server/Server.java
+++ kaiku_back/src/main/java/com/ryhma_3/kaiku/socket/server/Server.java
@@ -5,18 +5,34 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIONamespace;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
 import com.ryhma_3.kaiku.model.cast_object.AuthObject;
+import com.ryhma_3.kaiku.model.cast_object.ChatObject;
+import com.ryhma_3.kaiku.model.cast_object.MessageObject;
+import com.ryhma_3.kaiku.model.cast_object.UserStatusObject;
+import com.ryhma_3.kaiku.model.database.ChatDAO;
 import com.ryhma_3.kaiku.model.database.UserDAO;
 import com.ryhma_3.kaiku.socket.init.IServerInit;
 import com.ryhma_3.kaiku.utility.SecurityTools;
 import com.ryhma_3.kaiku.utility.Token;
 
+/**
+ * @author Panu Lindqvist
+ * A socket server that implements IServer. Contains listeners to listen incoming connections and messages and has 
+ * multiplexing functionality to facilitate unnumbered amount of chats.
+ * Collects incoming traffic and utilises database Data Access Objects.
+ */
 public class Server implements IServer {
 	
 	private static final Map<String, Boolean> connectedUsers = new HashMap<>(); 
+	private static final ArrayList<SocketIONamespace> namespaces = new ArrayList<>();
+//	ChatDAO chatDAO = new ChatDAO();
 	final SocketIOServer server;
 	IServerInit init;
 
@@ -26,39 +42,130 @@ public class Server implements IServer {
 	}
 	
 	@Override
-	public void start() {
+	public void start() {		
+		
+		//after boot create namespaces for existing chats
+		initializeFromDatabase(server);
+
+		//Gatekeep and register incoming connections
 		server.addConnectListener(new ConnectListener() {			
 			@Override
 			public void onConnect(SocketIOClient client) {
+				System.out.println("connect event");
 				
 				//Check connection authentication
-				String tokenString = client.getHandshakeData().getHttpHeaders().get("auth");
-				boolean approved = SecurityTools.connectTokenToUUID(tokenString, client.getSessionId());
+				String tokenString = client.getHandshakeData().getHttpHeaders().get("Authorization");
+				System.out.println(tokenString);
+				boolean approved = SecurityTools.verifySession(tokenString, client.getSessionId());
 				
 				//kick if not authenticated
 				if(!approved) {
 					
-					client.disconnect();
+					//client.disconnect();
 					
 				} else {
+					
+					Token cloneOfToken = SecurityTools.getCloneOfToken(tokenString);
+					
 					//update connectedUsers
-					Boolean isSet = connectedUsers.putIfAbsent(SecurityTools.getCloneOfToken(tokenString).getUser_id(), true);
+					Boolean isSet = connectedUsers.putIfAbsent(cloneOfToken.getUser_id(), true);
 					if(isSet==null) {
-						connectedUsers.put(SecurityTools.getCloneOfToken(tokenString).getUser_id(), true);
+						connectedUsers.replace(cloneOfToken.getUser_id(), true);
 					}
 					
 					//send client current user statuses
 					client.sendEvent("connect", connectedUsers);
+					
+					//update other clients about this user
+					server.getBroadcastOperations().sendEvent("connectionEvent", new UserStatusObject(cloneOfToken.getUser_id(), true));
 				}
 			}
-			
-			
+		});
+		
+		
+		//register disconnections
+		server.addDisconnectListener(new DisconnectListener() {
+			@Override
+			public void onDisconnect(SocketIOClient client) {
+				String tokenString = client.getHandshakeData().getHttpHeaders().get("Authorization");
+				Token cloneOfToken = SecurityTools.getCloneOfToken(tokenString);
+				
+				//set user as disconnected
+				connectedUsers.replace(cloneOfToken.getUser_id(), false);
+				
+				//broadcast info
+				server.getBroadcastOperations().sendEvent("connectionEvent", new UserStatusObject(cloneOfToken.getUser_id(), true));
+			}
+		});
+		
+		
+		server.addEventListener("createChatEvent", ChatObject.class, new DataListener<ChatObject>() {
+			@Override
+			public void onData(SocketIOClient client, ChatObject data, AckRequest ackSender) throws Exception {
+//				ChatObject result = chatDAO.createChatObject(data);
+				ChatObject result = new ChatObject();
+				if(result != null) {
+					//create namespace
+					setupNamespace(server, result);
+					
+					//gather related users
+					Token[] users = new Token[data.getUsers().length];
+					for(int i=0; i<data.getUsers().length; i++) {
+						users[i] = SecurityTools.getCloneOfToken(data.getUsers()[i]);
+					}
+					
+					//send information event to related users
+					for (Token token : users) {
+						SocketIOClient receiver = server.getClient(token.getSessionID());
+						receiver.sendEvent("createChatEvent", data);
+					}
+					
+				} else {
+					client.sendEvent("fail");
+				}
+			}
+		});
+		
+		server.start();
+	}
+
+	/**
+	 * @param server
+	 * Collect all chats from database and add them into servers as namespaces
+	 */
+	private void initializeFromDatabase(SocketIOServer server) {
+		//add admin
+		SecurityTools.createOrUpdateToken("admin", "admin");
+		
+		//TODO initialisation form database
+//		ChatObject[] chats = ChatDAO.getChats(); //all
+//		for (ChatObject chatObject : chats) {
+//			setupNamespace(server, chatObject);
+//		}
+	}
+	
+	
+	/**
+	 * @param server
+	 * @param chatObject
+	 * Running chat objects through this method creates new server namespaces with proper attributes attached.
+	 */ 
+	private void setupNamespace(SocketIOServer server, ChatObject chatObject) {
+		SocketIONamespace namespace = server.addNamespace("/" + chatObject.getChat_id());
+		namespaces.add(namespace);
+		
+		namespace.addEventListener("chatEvent", MessageObject.class, new DataListener<MessageObject>() {
+			@Override
+			public void onData(SocketIOClient client, MessageObject data, AckRequest ackSender) throws Exception {
+				//MessageDAO.createMessage(data,chatObject.getChat_id());
+				namespace.getBroadcastOperations().sendEvent("chatEvent", data);
+			}
 		});
 	}
-
+	
+	
 	@Override
 	public void stopServer() {
-		
+		server.stop();
 	}
-
 }
