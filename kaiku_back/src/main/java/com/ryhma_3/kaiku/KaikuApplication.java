2a3c0fb87f6c47076d505d189aebb933df97112d
Panu Lidnqvist <panu.lindqvist@gmail.com>
2020-02-25 15:36:08 +0200

    put 'no-auth' config to main exec

--- kaiku_back/src/main/java/com/ryhma_3/kaiku/KaikuApplication.java
+++ kaiku_back/src/main/java/com/ryhma_3/kaiku/KaikuApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.ryhma_3.kaiku.socket.init.ChatServerInit;
 import com.ryhma_3.kaiku.socket.init.IServerInit;
+import com.ryhma_3.kaiku.socket.init.ServerInitAuth;
+import com.ryhma_3.kaiku.socket.init.ServerInitNoAuth;
 import com.ryhma_3.kaiku.socket.server.Server;
 
 @SpringBootApplication
@@ -14,7 +16,7 @@ public class KaikuApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KaikuApplication.class, args);
 		
-		IServerInit init = new ChatServerInit();
+		IServerInit init = new ServerInitNoAuth();
 		Server server = new Server(init);
 		
 		server.start();
