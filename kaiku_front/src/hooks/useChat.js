247e3025ad245d60cfede0a449ad2f6d1d46ad08
kaikuh <leevi.niemi@metropolia.fi>
2020-02-27 15:17:45 +0200

    testing new functions for socket io

--- kaiku_front/src/hooks/useChat.js
+++ kaiku_front/src/hooks/useChat.js
@@ -1,15 +1,21 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState, useContext } from "react"
 import socketIOClient from 'socket.io-client'
 import jsonService from '../services/jsonService'
+import InitialData from "../providers/InitialData"
 
-const useChat = (loggedUser_id) => {
+const useChat = (loggedUser_id, currentChat) => {
+  const { initialData } = useContext(InitialData)
   const [messages, setMessages] = useState([])
-  const socketRef = useRef();
+   const socketRef = useRef()
+  useEffect(() => {
+    if (initialData.chats === undefined) return console.log('initialData pending...')
+    setMessages(initialData.chats[0].messages)
+  }, [initialData])
 
   useEffect(() => {
-    jsonService.getMessages()
-      .then(messages => setMessages(messages))
-  }, [])
+    if (currentChat === undefined) return 
+    setMessages(currentChat.messages)
+  }, [currentChat])
 
   useEffect(() => {
     socketRef.current = socketIOClient(
@@ -17,9 +23,30 @@ const useChat = (loggedUser_id) => {
     )
 
     socketRef.current.on(
-      "chatevent",
+      "chatEvent",
       (message) => {
-        if (message.user_id !== loggedUser_id) setMessages(messages.concat(message))
+        if (message.user_id === loggedUser_id) return
+
+        //Toiminnot vastaanottajalle
+        setMessages(messages.concat(message))
+      }
+    )
+
+    socketRef.current.on(
+      "connect",
+      (users) => {
+	console.log(users)
+	// 	socket = socketIO.connect(baseString, {
+	//   query: "Authorization=kaiku"
+	// });
+      }
+    )
+
+    socketRef.current.on(
+      "createChatEvent",
+      (chat) => {
+	console.log('createChatEvent')
+        console.log(chat)
       }
     )
 
@@ -28,10 +55,14 @@ const useChat = (loggedUser_id) => {
     }
   }, [messages])
 
+  const createChatEvent = () => {
+    socketRef.current.emit("chat", { chatName: 'testichat' , users: ['1','2','3'] })
+  }
+
   const sendMessage = (message) => {
     //    socketRef.current.emit("chatevent", { message: message.content, user: 'Minä' })
     setMessages(messages.concat(message))
-    socketRef.current.emit("chatevent", message)
+    socketRef.current.emit("chatEvent", message)
   }
   return { messages, sendMessage }
 }
