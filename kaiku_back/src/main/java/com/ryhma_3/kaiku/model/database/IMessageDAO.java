7823dfbaa8f460145463de7340231d97354b2022
jarr <jesse@arola.fi>
2020-03-09 00:36:18 +0200

    Added Javadocs

--- kaiku_back/src/main/java/com/ryhma_3/kaiku/model/database/IMessageDAO.java
+++ kaiku_back/src/main/java/com/ryhma_3/kaiku/model/database/IMessageDAO.java
@@ -3,12 +3,45 @@ package com.ryhma_3.kaiku.model.database;
 import com.ryhma_3.kaiku.model.cast_object.MessageObject;
 
 /**
- * IMessageDAO
+ * Data Access Object for messages
  */
 public interface IMessageDAO {
+
+    /**
+     * Creates a message entry in the database
+     * @param messageObject
+     * @param chat_id
+     * @return Created message entry
+     */
     public MessageObject createMessage(MessageObject messageObject, String chat_id);
+
+    /**
+     * Updates a message entry in the database
+     * @param messageObject
+     * @param chat_id
+     * @return Updated message entry
+     */
     public MessageObject updateMessage(MessageObject messageObject, String chat_id);
+
+    /**
+     * Deletes a message entry from the database
+     * @param messageObject
+     * @param chat_id
+     */
     public void deleteMessage(MessageObject messageObject, String chat_id);
+
+    /**
+     * Gets a single message entry from the database
+     * @param messageObject
+     * @param chat_id
+     * @return Message entry
+     */
     public MessageObject getMessage(MessageObject messageObject, String chat_id);
+
+    /**
+     * Gets all message entries of a specific user
+     * @param user_id
+     * @return All message entries of a specified user
+     */
     public MessageObject[] getAllMessages(String user_id);
 }
--- kaiku_back/src/main/java/com/ryhma_3/kaiku/model/database/IUserDAO.java
+++ kaiku_back/src/main/java/com/ryhma_3/kaiku/model/database/IUserDAO.java
@@ -3,14 +3,42 @@ package com.ryhma_3.kaiku.model.database;
 import com.ryhma_3.kaiku.model.cast_object.UserObject;
 
 /**
- * IAccountsDAO
+ * Data Access Object for Users
  */
 public interface IUserDAO {
 
+    /**
+     * Creates a user entry in the database
+     * @param profileObject
+     * @return Created user
+     */
     public UserObject createUser(UserObject profileObject);
+
+    /**
+     * Updates a specific user in the database
+     * @param profileObject
+     * @return Updated user
+     */
     public UserObject updateUser(UserObject profileObject);
+
+    /**
+     * Deletes a specific user entry from the database
+     * @param profileObject
+     * @return True if successful
+     */
     public boolean deleteUser(UserObject profileObject);
+
+    /**
+     * Gets a specific user from the database
+     * @param profileObject
+     * @return Fetched user entry
+     */
     public UserObject getUser(UserObject profileObject);
+
+	/**
+     * Gets all users across the whole database
+	 * @return List containing all users
+	 */
 	public UserObject[] getAllUsers();
 
 }
