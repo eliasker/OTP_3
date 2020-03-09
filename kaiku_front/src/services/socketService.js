ade343e88140a30e6a784561f501d2927af7f52b
eliasker <eliasker@gmail.com>
2020-03-09 16:14:55 +0200

    updated services

--- kaiku_front/src/services/socketService.js
+++ kaiku_front/src/services/socketService.js
@@ -1,42 +1,42 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 import socketIOClient from 'socket.io-client';
 
 const SocketService = () => {
 
-  const[socket, setSocket] = useState()
+  const [socket, setSocket] = useState()
 
   const createSocketConnection = (token) => {
     const socketUrl = 'http://localhost:9991';
 
     console.log('token: ', token);
-    
+
     const _socket = socketIOClient.connect(
       socketUrl,
-      {query: `Authorization=${token}`}
+      { query: `Authorization=${token}` }
     )
 
-    _socket.on('connect', function(data){
+    _socket.on('connect', function (data) {
       console.log('connect event');
       console.log('connected users: ', data);
     });
-    
-    _socket.on('chatEvent', function(data){
+
+    _socket.on('chatEvent', function (data) {
       console.log('chatEvent received', data);
     });
-    
-    _socket.on('disconnect', function(){
+
+    _socket.on('disconnect', function () {
       console.log('disconnect');
     });
 
-    _socket.on('createChatEvent', function(data){
+    _socket.on('createChatEvent', function (data) {
       console.log('createChatEvent', data);
     })
-
+    console.log('_socket', _socket)
     setSocket(_socket);
   }
 
   const sendMessage = (message, user_id, chat_id) => {
-    
+
     const obj = {
       content: message,
       user_id,
@@ -47,7 +47,7 @@ const SocketService = () => {
   }
 
   const createChat = (chatName, type, users) => {
-
+    console.log('creating chat socket service')
     const obj = {
       chatName,
       type,
@@ -55,7 +55,7 @@ const SocketService = () => {
     }
 
     console.log("create: ", obj);
-    
+    console.log(socket)
 
     socket.emit("createChatEvent", obj);
   }
