fc4e7a10905801fb8943465eeb44ab19269ab972
wesenbergg <b.b.j.1998@gmail.com>
2020-03-09 15:01:13 +0200

    style changes

--- kaiku_front/src/util/help.js
+++ kaiku_front/src/util/help.js
@@ -5,8 +5,8 @@ const help = [
   {title: 'Profiilin päivittäminen', content: 'Voit muokata profiilia muokkaussivulla näpyttämällä tietoja valmiiksi täytettyihin kenttiin. Paina lopuksi Tallenna-painiketta ottakseen käyttöön muokatut tiedot.'},
   {title: 'Keskustelun aloitus', content: 'Keskustelunäkymä näyttää jo aloitetut yksityiset keskustelut. Aloittaksesi uuden keskustelun paina ryhmänäkymästä henkilön nimeä tai etsi henkilö hakukentän avulla.'},
   {title: 'Profiilien tarkastelu', content: 'Voit tarkastella muiden profiileja klikkaamalla ryhmäkeskustelunäkymästä haluttua profiilia ja valitsemalla "Näytä profiili".'},
-  {title: 'Paneelit osa 1', content: 'Profiilinmuokkaussivu, käyttäjäprofiilien tarkastelu ja chatit ovat päällekkäisiä paneeleita (lueteltu päällimmäisestä alimpaan).'},
-  {title: 'Paneelit osa 2', content: 'Jotta pääsisit pällimäisestä paneelista alempaan, on päällimäisempi paneeli suljettava.'},
+  {title: 'Paneelit (osa 1)', content: 'Profiilinmuokkaussivu, käyttäjäprofiilien tarkastelu ja chatit ovat päällekkäisiä paneeleita (lueteltu päällimmäisestä alimpaan).'},
+  {title: 'Paneelit (osa 2)', content: 'Jotta pääsisit pällimäisestä paneelista alempaan, on päällimäisempi paneeli suljettava.'},
   {title: 'Apua paneeli', content: 'Psst! Löydät tarvittaessa minut myös päävalikosta.'},
   {title: 'Piilota "Apua-nappi"', content: 'Saat piilotettua \'apua\' -painikkeen klikkaamalla piilota-painiketta.'}
 ]
