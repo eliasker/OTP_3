db2137872835ae70bb850ae0a8fed610ff65d9c8
eliasker <eliasker@gmail.com>
2020-03-10 19:09:47 +0200

    services updated, fixed warnings

--- kaiku_front/src/components/chat/ChatHeader.js
+++ kaiku_front/src/components/chat/ChatHeader.js
@@ -3,16 +3,16 @@ import CurrentChat from '../../providers/CurrentChat'
 
 const ChatHeader = ({ searchInput, setSearchInput }) => {
   const { currentChat } = useContext(CurrentChat)
-
+  if (currentChat === null || currentChat === undefined) return (<></>)
   return (
     <div className="chat-header row justify-content-between bg-primary-0">
       <div className="row marginy-0">
-        {currentChat !== null ?
-          <img src={currentChat.image ? currentChat.image: '/kaikuthumb.png'}
+        {(currentChat !== null && currentChat.image !== undefined) ?
+          <img src={currentChat.image ? currentChat.image : '/kaikuthumb.png'}
             alt="profiilikuva" className="profile-thumb alpha-1" className={`${currentChat.color} profile-thumb`} /> :
           <img src="/kaikuthumb.png" alt="chatin kuva" className="profile-thumb alpha-1" />
         }
-        <p className="d-none d-lg-block">{currentChat === null ? '' : currentChat.name}</p>
+        <p className="d-none d-lg-block">{currentChat.type === 'private' ? currentChat.name : currentChat.chatName}</p>
       </div>
       <form id="search-message" onSubmit={e => e.preventDefault()}>
         <input type="search" placeholder="search" value={searchInput} onChange={e => setSearchInput(e.target.value)} />
