dde3b639d6b6cbf04ad84e59adf54cab1bb148d3
wesenbergg <b.b.j.1998@gmail.com>
2020-03-19 23:42:02 +0200

    localization init

---
+++ kaiku_front/src/util/localization.js
@@ -0,0 +1,62 @@
+export default (
+  {
+    fi: {
+      /**Profile update page */
+      exit: 'Poistu',
+      password_placeholder: 'Salasana',
+      re_password_placeholder: 'Varmenna salasana',
+      save_button: 'Tallenna',
+      /**Chat page */
+      users_online: 'Käyttäjiä paikalla - ',
+      menu_profile: 'Profiili',
+      menu_help: 'Apua',
+      menu_logout: 'Kirjaudu ulos',
+      user_search_placeholder: 'Etsi käyttäjä',
+      message_search_placeholder: 'Etsi...',
+      message_form_placeholder: 'Lähetä viesti',
+      /** Help */
+      next_button: 'Seuraava',
+      prev_button: 'Edellinen',
+      hide_button: 'Piilota',
+      help_tips: [
+        {title: 'Kaikki käyttäjät', content: 'Voit löytää kaikki käyttäjät Kaiku-ryhmächatista.'},
+        {title: 'Päävalikko', content: 'Päävalikon voit löytää profiili kuvan oikealta puolelta.'},
+        {title: 'Profiilin muokkaussivu', content: 'Pääset profiilinmuokkaussivulle klikkaamalla omaa profiilikuvaa vasemmasta yläkulmasta tai päävalikosta'},
+        {title: 'Profiilin päivittäminen', content: 'Voit muokata profiilia muokkaussivulla näpyttämällä tietoja valmiiksi täytettyihin kenttiin. Paina lopuksi Tallenna-painiketta ottakseen käyttöön muokatut tiedot.'},
+        {title: 'Keskustelun aloitus', content: 'Keskustelunäkymä näyttää jo aloitetut yksityiset keskustelut. Aloittaksesi uuden keskustelun paina ryhmänäkymästä henkilön nimeä tai etsi henkilö hakukentän avulla.'},
+        {title: 'Profiilien tarkastelu', content: 'Voit tarkastella muiden profiileja klikkaamalla ryhmäkeskustelunäkymästä haluttua profiilia ja valitsemalla "Näytä profiili".'},
+        {title: 'Paneelit (osa 1)', content: 'Profiilinmuokkaussivu, käyttäjäprofiilien tarkastelu ja chatit ovat päällekkäisiä paneeleita (lueteltu päällimmäisestä alimpaan).'},
+        {title: 'Paneelit (osa 2)', content: 'Jotta pääsisit pällimäisestä paneelista alempaan, on päällimäisempi paneeli suljettava.'},
+        {title: 'Apua paneeli', content: 'Psst! Löydät tarvittaessa minut myös päävalikosta.'},
+        {title: 'Piilota "Apua-nappi"', content: 'Saat piilotettua \'apua\' -painikkeen klikkaamalla piilota-painiketta.'}
+      ]
+      /** Profile page */
+      /** Login page */
+      /** Dashboard */
+    },
+    en: {
+      /** Profile update page */
+      /** Chat page */
+      /** Help */
+      /** Profile page */
+      /** Login page */
+      /** Dashboard */
+    },
+    ru: {
+      /** Profile update page */
+      /** Chat page */
+      /** Help */
+      /** Profile page */
+      /** Login page */
+      /** Dashboard */
+    },
+    jp: {
+      /** Profile update page */
+      /** Chat page */
+      /** Help */
+      /** Profile page */
+      /** Login page */
+      /** Dashboard */
+    }
+  }
+)
