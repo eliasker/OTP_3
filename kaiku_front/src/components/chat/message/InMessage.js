02137e10ab4ea247ac5aa3b153e1a51f7567786c
Elias <eliasker@gmail.com>
2020-04-17 14:13:49 +0300

    timestamps fixed

--- kaiku_front/src/components/chat/message/InMessage.js
+++ kaiku_front/src/components/chat/message/InMessage.js
@@ -1,11 +1,7 @@
-import React, { useContext } from 'react'
-import formatTime from '../../../util/formatTime'
-import InitialData from '../../../providers/InitialData';
+import React from 'react'
+import Timestamp from './Timestamp'
 
 const InMessage = ({ message, user }) => {
-  const { useLang } = useContext(InitialData);
-  const string = (ref) => useLang.getString(ref)
-
   if (user === undefined) user = { color: 'green', username: '' }
   return (
     <div className="in-container">
@@ -14,7 +10,7 @@ const InMessage = ({ message, user }) => {
           <span className={`${user.color} msg-senda bg-none`}>{user.username}</span>
           <br />{message.content}
         </p>
-        <span className="in-message-date">{string('time_today') + formatTime.formatTimeStamp(message.timestamp)}</span>
+        <Timestamp timestamp={message.timestamp} inMessage={true} />
       </div>
     </div>
   )
--- kaiku_front/src/components/chat/message/OutMessage.js
+++ kaiku_front/src/components/chat/message/OutMessage.js
@@ -1,15 +1,12 @@
-import React, { useContext } from 'react'
-import formatTime from '../../../util/formatTime'
-import InitialData from '../../../providers/InitialData'
+import React from 'react'
+import Timestamp from './Timestamp'
 
 const OutMessage = ({ message }) => {
-  const { useLang } = useContext(InitialData)
-  const string = (ref) => useLang.getStrign
   return (
     <div className="out-container">
       <div className="out-message">
         <p>{message.content}</p>
-        <span className="out-message-date">{string('time_today') + formatTime.formatTimeStamp(message.timestamp)}</span>
+        <Timestamp timestamp={message.timestamp} inMessage={false} />
       </div>
     </div>
   )
