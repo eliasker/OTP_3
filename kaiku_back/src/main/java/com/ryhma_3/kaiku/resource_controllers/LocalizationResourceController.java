e30cd80643edff0ad29f11bb9f196254141104dd
jarr <jesse@arola.fi>
2020-04-20 11:06:31 +0300

    Added missing javadoc strings

--- kaiku_back/src/main/java/com/ryhma_3/kaiku/resource_controllers/LocalizationResourceController.java
+++ kaiku_back/src/main/java/com/ryhma_3/kaiku/resource_controllers/LocalizationResourceController.java
@@ -9,17 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ryhma_3.kaiku.KaikuApplication;
-import com.ryhma_3.kaiku.model.cast_object.ChatObject;
 import com.ryhma_3.kaiku.model.cast_object.LocalizationObject;
-import com.ryhma_3.kaiku.model.database.IAdminDAO;
-import com.ryhma_3.kaiku.model.database.IChatDAO;
-import com.ryhma_3.kaiku.model.database.IMessageDAO;
 import com.ryhma_3.kaiku.model.database.ILocalizationDAO;
 import com.ryhma_3.kaiku.resource_controllers.exceptions.BadUserInputException;
 import com.ryhma_3.kaiku.resource_controllers.exceptions.ResourceNotFoundException;
 import com.ryhma_3.kaiku.resource_controllers.exceptions.ValidationFailedException;
-import com.ryhma_3.kaiku.utility.Logger;
-import com.ryhma_3.kaiku.utility.SecurityTools;
 
 import static com.ryhma_3.kaiku.utility.Logger.log;
 
@@ -27,6 +21,7 @@ import java.util.ArrayList;
 
 /**
  * LocalizationResourceController
+ * Controller to access application localization strings via API
  */
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -34,6 +29,11 @@ public class LocalizationResourceController {
 
     private ILocalizationDAO localizationDAO = KaikuApplication.getLocalizationDAO();
 
+    /**
+     * Gets the localization strings for a language from the database.
+     * @param identicator
+     * @return The {@link LocalizationObject} containing strings of a certain locale.
+     */
     @RequestMapping(value = "/api/locale", method = RequestMethod.GET)
     public LocalizationObject getLocale(@RequestParam String identicator) {
         log("REST: Localization object GET request");
@@ -45,6 +45,10 @@ public class LocalizationResourceController {
         throw new ResourceNotFoundException();
     }
 
+    /**
+     * Gets the list of indicators strings for each language in the database.
+     * @return List of indicators of all locales in the database.
+     */
     @RequestMapping(value = "/api/locale/indicators", method = RequestMethod.GET)
     public ArrayList<String> getLocaleIndicators() {
         log("REST: Localization indicators GET request");
@@ -56,6 +60,12 @@ public class LocalizationResourceController {
         throw new ResourceNotFoundException();
     }
     
+    /**
+     * Adds a language in the localization database.
+     * @param localizationObject
+     * @param token Usertoken for admin privilige checks.
+     * @return Added {@link LocalizationObject}.
+     */
     @RequestMapping(value = "/api/locale", method = RequestMethod.POST)
     public LocalizationObject putLocalizationObject(
             @RequestBody LocalizationObject localizationObject,
@@ -75,6 +85,12 @@ public class LocalizationResourceController {
         throw new ValidationFailedException();
     }
 
+    /**
+     * Adds a language in the localization database.
+     * @param identicator
+     * @param token Usertoken for admin privilige checks.
+     * @return True if successful.
+     */
     @RequestMapping(value = "/api/locale", method = RequestMethod.DELETE)
     public boolean putLocalization(
             @RequestParam String identicator,
