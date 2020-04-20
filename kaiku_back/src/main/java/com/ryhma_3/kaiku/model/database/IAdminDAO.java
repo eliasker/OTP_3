88f89cc62e702795799d600ad594f497c757a444
jarr <jesse@arola.fi>
2020-04-20 11:45:24 +0300

    Added missing javadocs

--- kaiku_back/src/main/java/com/ryhma_3/kaiku/model/database/IAdminDAO.java
+++ kaiku_back/src/main/java/com/ryhma_3/kaiku/model/database/IAdminDAO.java
@@ -5,8 +5,23 @@ package com.ryhma_3.kaiku.model.database;
  */
 public interface IAdminDAO {
 
+    /**
+     * Adds a user_id in the admins collection.
+     * @param id user_id of the user to be added.
+     * @return true if succesful.
+     */
     public boolean addId(String id);
+
+    /**
+     * @param id user_id of the user to be queried.
+     * @return true if succesful.
+     */
     public boolean getId(String id);
+
+    /**
+     * @param id user_id of the user to be deleted.
+     * @return true if succesful.
+     */
     public boolean deleteId(String id);
 
 }
