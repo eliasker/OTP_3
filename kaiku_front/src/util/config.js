072fb4fd05d7aa4c76b229276ac9b6a80159437b
Puune <panu.lindqvist@gmail.com>
2020-05-01 10:17:40 +0300

    testing

--- kaiku_front/src/util/config.js
+++ kaiku_front/src/util/config.js
@@ -1,3 +1,3 @@
-const BASEURI = 'http://10.114.32.19:8081/' //'http://localhost:8080/'
-const SOCKETURI = 'http://10.114.32.19:8083/'
+const BASEURI = 'http://localhost:8081' //'http://10.114.32.19:8081/' //'http://localhost:8080/'
+const SOCKETURI = 'http://localhost:8083' //'http://10.114.32.19:8083/'
 export default { BASEURI, SOCKETURI }
